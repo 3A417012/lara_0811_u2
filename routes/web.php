0865ecf98513a88a55be391e2648ab1d6c9bd47d
3A417012 <nds10745@gmail.com>
2018-10-28 03:51:27 +0800

    Adding prefix routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Route::get('say/{name?}',['as' => 'hello.index',function($name = 'Everybody'){
 	return'Hello '.$name;
 }]);
+/*
+Route::get('dashboard',function(){
+	return'dashboard';
+});
+*/
+Route::group(['prefix'=>'admin'],function(){
+	Route::get('dashboard',function(){
+		return'admin dashboard';
+	});
+});
+
